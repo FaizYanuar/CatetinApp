01f2132ce82fdf7394f0d5c883a17d41929acf79
FaizYanuar <fyigm23@gmail.com>
2025-06-04 12:09:18 +0700

    feat: add barchart dashboard

--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -2,17 +2,26 @@
 import { UserButton, useUser } from '@clerk/nextjs'
 import React from 'react'
 import DashboardCard from './_components/dashboardCard';
+import DashboardBarChart from './_components/dashboardBarChart';
 
 function Dashboard() {
   const {user}=useUser();
   return (
     <div className='bg-[#DEDFEC] h-screen'>
         <div className='p-5'>
-            <h1 className='font-semibold'>| <span className='text-blue-800'>Dashboard</span></h1>
-            <h1 className='mt-5 text-xl font-semibold'>Hello, {user?.fullName}! <span className='text-2xl'>👋</span></h1>
+            <h1 className='font-semibold text-blue-800'>
+              Dashboard <span className='text-black text-xl'>
+                
+                | Hello, {user?.fullName}! 
+                
+                <span className='text-2xl'>
+                  👋
+                  
+                  </span></span></h1>
         </div>
 
         <DashboardCard />
+        <DashboardBarChart/>
     </div>
   )
 }
